100f21d166fdf112e8ed7f5e52a5a81262766b49
ravishankar <ravishankar.r1@gmail.com>
2017-11-05 19:43:50 -0800

    Extension Methods

---
+++ extension-methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person { Name = "John", Age = 33 };
+            var p2 = new Person { Name = "Jane", Age = 27 };
+            p1.SayHello(p2);
+
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void SayHello(this Person person1, Person person2)
+        {
+            Console.WriteLine("{0} says hello to {1} ",person1.Name, person2.Name);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
